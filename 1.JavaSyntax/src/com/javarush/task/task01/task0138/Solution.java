0456bbef84aea015db4846c0fd6e2630e01aa71b
awecea <awecea@gmail.com>
2017-02-20 22:12:04 +0200

    add tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        //напишите тут ваш код
+        System.out.println("Духовной жаждою томим,");
+        System.out.println("В пустыне мрачной я влачился,");
+        System.out.println("И шестикрылый серафим");
+        System.out.println("На перепутье мне явился.");
+    }
+}
