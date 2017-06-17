b213f5e11695911ca31b10d8004873a1e1fd3247
awecea <awecea@gmail.com>
2017-06-17 22:56:53 +0300

    add task 3608 level 6

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/UsersView.java
@@ -9,11 +9,15 @@ public class UsersView implements View {
 
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All users:");
+        if (modelData.isDisplayDeletedUserList()) {
+            System.out.println("All deleted users:");
+        } else {
+            System.out.println("All users:");
+        }
         for (User user : modelData.getUsers()) {
             System.out.println(String.format("\tUser{name='%s', id=%d, level=%d}", user.getName(), user.getId(), user.getLevel()));
         }
-        System.out.println("==================================================");
+        System.out.println("===================================================");
     }
 
     @Override
